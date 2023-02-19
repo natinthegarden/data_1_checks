b1477d323034bf343172af12beea858a6de28564
natalya naser <nat.naser@gmail.com>
2023-02-19 03:28:24 -0500

    add data.py

---
+++ data.py
@@ -0,0 +1,18 @@
+#print hello world
+
+print("Hello World")
+
+#list
+
+list = ["cat", "dog", "monkey"]
+print (2)
+
+#dictionary
+
+dictionary = { "spring": "rain", "summer": "sun"}
+print(dictionary)
+
+#tuple
+
+tuple = ("milk", "eggs", "flour", "sugar")
+print(tuple[1])
